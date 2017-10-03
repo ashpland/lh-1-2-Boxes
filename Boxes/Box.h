c1c918af17bada3c734a3ac728bf5b29b3163634
Andrew Shopland <hi@ashp.land>
2017-10-03 10:51:23 -0700

    Box.h properties and initializers

---
+++ Boxes/Box.h
@@ -0,0 +1,19 @@
+//
+//  Box.h
+//  Boxes
+//
+//  Created by Andrew on 2017-10-03.
+//  Copyright © 2017 Andrew. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Box : NSObject
+
+@property float height;
+@property float width;
+@property float length;
+
+-(instancetype)initWithBoxHeight:(float)height boxWidth:(float)width boxLength:(float)length;
+
+@end
---
+++ Boxes/Box.m
@@ -0,0 +1,13 @@
+//
+//  Box.m
+//  Boxes
+//
+//  Created by Andrew on 2017-10-03.
+//  Copyright © 2017 Andrew. All rights reserved.
+//
+
+#import "Box.h"
+
+@implementation Box
+
+@end
